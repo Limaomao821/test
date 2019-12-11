c1a4685826f4a84137c7383a34435537665391bc
Limaomao821 <1114320079@qq.com>
2019-12-11 09:49:24 +0800

    switchCapslock.sh: show Capslock status

--- script/switchCaps.sh
+++ script/switchCaps.sh
@@ -7,5 +7,18 @@
 #   your Capslock status
 #   USAGE##################################
 
-output = xset q | grep 'Caps Lock' | awk '{ print $NF }'
-echo "$output"
+output=`xset q | grep 'Caps Lock:   o[nf]*' -o | grep 'o[nf]\+' -o`
+case $output in
+    'on')
+        killall notify-osd
+        notify-send -t 1000 "Capslock ON"
+        sleep .5 
+        killall notify-osd
+        ;;
+    'off')
+        killall notify-osd
+        notify-send -t 1000 "Capslock OFF"
+        sleep .5
+        killall notify-osd
+        ;;
+esac
